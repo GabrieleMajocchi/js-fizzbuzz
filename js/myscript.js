52e979ba2325d073d04035d1cbdddb9505482d83
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-16 17:13:51 +0200

    bonus rainbow background

--- js/myscript.js
+++ js/myscript.js
@@ -1,44 +1,17 @@
 
 const ul = document.querySelector('ul');
 
-var bgcolor = document.getElementsByClassName('rainbow')['0'];
+var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-var r = 0;
-var g = 0;
-var b = 0;
+		var i = 1;
 
-window.onload = function rainbow() {
-
-	if (r <= 255 && g == 0 && b == 0) {
-		r ++;
-	}
-
-	if (r == 255 && b == 0 && g <= 255) {
-		g ++;
-	}
-
-	if (r == 255 && g == 255 && b <= 255) {
-		b ++;
-	}
-
-	if (b == 255 && g == 255 && r > 0) {
-		r --;
-	}
-
-	if (r == 0 && b == 255 && g > 0) {
-		g --;
-	}
-
-	if (r == 0 && g == 0 && b > 0) {
-		b --;
-	}
-
-	setTimeout(function() {
-		rainbow();
-	}, 10);
-
-	bgcolor.style.background = 'rgb('+r+','+g+','+b+')';
-}
+		window.setInterval(function(){
+			document.body.style.backgroundColor = colors[i];
+			i++;
+			if (i === colors.length){
+				i=0;
+			}
+		}, 1000);
 
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
