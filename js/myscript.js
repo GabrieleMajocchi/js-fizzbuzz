48cb7a2b9df4cd2d7a11b8c61cefdc88518ff286
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-16 15:30:30 +0200

    added elements for the bonus 2

--- js/myscript.js
+++ js/myscript.js
@@ -5,16 +5,21 @@ for(let i = 1 ; i < 101 ; i++){
     if ((i % 3 === 0) && (i % 5 === 0)){
         const li = document.createElement('li');
         li.append('FizzBuzz');
+        li.classList.add('fizzbuzz');
         ul.append(li);
+        
     }
     else if (i % 3 === 0){
         const li = document.createElement('li');
         li.append('Fizz');
+        li.classList.add('fizz');
         ul.append(li);
+        
     }
     else if (i % 5 === 0){
         const li = document.createElement('li');
         li.append('Buzz');
+        li.classList.add('buzz');
         ul.append(li);
     }
     else{
