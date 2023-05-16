28dd93dde6cdeae62bf50dc3f3c34a92e527435a
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-16 15:26:25 +0200

    removed console log

--- js/myscript.js
+++ js/myscript.js
@@ -6,24 +6,20 @@ for(let i = 1 ; i < 101 ; i++){
         const li = document.createElement('li');
         li.append('FizzBuzz');
         ul.append(li);
-        console.log(i);
     }
     else if (i % 3 === 0){
         const li = document.createElement('li');
         li.append('Fizz');
         ul.append(li);
-        console.log(i);
     }
     else if (i % 5 === 0){
         const li = document.createElement('li');
         li.append('Buzz');
         ul.append(li);
-        console.log(i);
     }
     else{
         const li = document.createElement('li');
         li.append(i);
         ul.append(li);
-        console.log(i);
     }
 }
