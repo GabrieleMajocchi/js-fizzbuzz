4315165c8a5b899b16533d88e7d4e4d152599056
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-16 16:15:01 +0200

    added a delay when printing the resoults

--- js/myscript.js
+++ js/myscript.js
@@ -1,30 +1,39 @@
 
 const ul = document.querySelector('ul');
 
-for(let i = 1 ; i < 101 ; i++){
-    if ((i % 3 === 0) && (i % 5 === 0)){
-        const li = document.createElement('li');
-        li.append('FizzBuzz');
-        li.classList.add('fizzbuzz');
-        ul.append(li);
-        
-    }
-    else if (i % 3 === 0){
-        const li = document.createElement('li');
-        li.append('Fizz');
-        li.classList.add('fizz');
-        ul.append(li);
-        
-    }
-    else if (i % 5 === 0){
-        const li = document.createElement('li');
-        li.append('Buzz');
-        li.classList.add('buzz');
-        ul.append(li);
-    }
-    else{
-        const li = document.createElement('li');
-        li.append(i);
-        ul.append(li);
-    }
-}
+
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+async function load () {
+                            for(let i = 1 ; i < 101 ; i++){
+                                if ((i % 3 === 0) && (i % 5 === 0)){
+                                    const li = document.createElement('li');
+                                    li.append('FizzBuzz');
+                                    li.classList.add('fizzbuzz');
+                                    ul.append(li);
+                                    await timer(150);
+                                }
+                                else if (i % 3 === 0){
+                                    const li = document.createElement('li');
+                                    li.append('Fizz');
+                                    li.classList.add('fizz');
+                                    ul.append(li);
+                                    await timer(150);
+                                }
+                                else if (i % 5 === 0){
+                                    const li = document.createElement('li');
+                                    li.append('Buzz');
+                                    li.classList.add('buzz');
+                                    ul.append(li);
+                                    await timer(150);
+                                }
+                                else{
+                                    const li = document.createElement('li');
+                                    li.append(i);
+                                    ul.append(li);
+                                    await timer(150);
+                                }
+                            }
+};
+
+load();
